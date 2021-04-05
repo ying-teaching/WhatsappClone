aec95650717864cf225dc678d037179d175bee9b
Ying Liu <yingliu4203@gmail.com>
2021-04-04 22:01:01 -0700

    add input message box

---
+++ src/components/InputBox/index.js
@@ -0,0 +1,90 @@
+import React, { useEffect, useState } from "react";
+import {
+  View,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  KeyboardAvoidingView,
+  Platform,
+} from "react-native";
+
+import styles from "./styles";
+
+import {
+  MaterialCommunityIcons,
+  MaterialIcons,
+  FontAwesome5,
+  Entypo,
+  Fontisto,
+} from "@expo/vector-icons";
+
+const InputBox = () => {
+  const [message, setMessage] = useState("");
+
+  const onMicrophonePress = () => {
+    console.warn("Record Voice");
+  };
+
+  const onSendPress = () => {
+    console.warn("Send Message");
+  };
+
+  const onPress = () => {
+    if (!message) {
+      onMicrophonePress();
+    } else {
+      onSendPress();
+      setMessage("");
+    }
+  };
+
+  return (
+    <KeyboardAvoidingView
+      behavior={Platform.OS == "ios" ? "padding" : "height"}
+      keyboardVerticalOffset={100}
+      style={{ width: "100%" }}
+    >
+      <View style={styles.container}>
+        <View style={styles.mainContainer}>
+          <FontAwesome5 name="laugh-beam" size={24} color="grey" />
+          <TextInput
+            placeholder={"Type a message"}
+            style={styles.textInput}
+            multiline
+            value={message}
+            onChangeText={setMessage}
+          />
+          <Entypo
+            name="attachment"
+            size={24}
+            color="grey"
+            style={styles.icon}
+          />
+          {!message && (
+            <Fontisto
+              name="camera"
+              size={24}
+              color="grey"
+              style={styles.icon}
+            />
+          )}
+        </View>
+        <TouchableOpacity onPress={onPress}>
+          <View style={styles.buttonContainer}>
+            {!message ? (
+              <MaterialCommunityIcons
+                name="microphone"
+                size={28}
+                color="white"
+              />
+            ) : (
+              <MaterialIcons name="send" size={28} color="white" />
+            )}
+          </View>
+        </TouchableOpacity>
+      </View>
+    </KeyboardAvoidingView>
+  );
+};
+
+export default InputBox;
--- src/screens/ChatRoomScreen.js
+++ src/screens/ChatRoomScreen.js
@@ -1,19 +1,26 @@
 import React from "react";
-import { FlatList } from "react-native";
+import { FlatList, ImageBackground } from "react-native";
 
 import { useRoute } from "@react-navigation/native";
 
 import chatRoomData from "../../data/Chats";
 import ChatMessage from "../components/ChatMessage";
+import BG from "../../assets/BG.png";
+import InputBox from "../components/InputBox";
 
 const ChatRoomScreen = () => {
   const route = useRoute();
 
   return (
-    <FlatList
-      data={chatRoomData.messages}
-      renderItem={({ item }) => <ChatMessage message={item} />}
-    />
+    <ImageBackground style={{ width: "100%", height: "100%" }} source={BG}>
+      <FlatList
+        data={chatRoomData.messages}
+        renderItem={({ item }) => <ChatMessage message={item} />}
+        inverted
+      />
+
+      <InputBox chatRoomID={route.params.id} />
+    </ImageBackground>
   );
 };
 
