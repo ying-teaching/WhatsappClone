f2945da51cde91d03f6e388bb9b7bacb8b9d3a56
Ying Liu <yingliu4203@gmail.com>
2021-04-05 19:07:09 -0700

    create chatroom

--- src/components/ContactListItem/index.js
+++ src/components/ContactListItem/index.js
@@ -5,11 +5,58 @@ import styles from "./styles";
 
 import { useNavigation } from "@react-navigation/native";
 
+import { API, graphqlOperation, Auth } from "aws-amplify";
+import { createChatRoom, createChatRoomUser } from "../../graphql/mutations";
+
 const ContactListItem = ({ user }) => {
   const navigation = useNavigation();
 
-  const onClick = () => {
-    console.warn("Chat with a user");
+  const onClick = async () => {
+    try {
+      //  1. Create a new Chat Room
+      const newChatRoomData = await API.graphql(
+        graphqlOperation(createChatRoom, {
+          input: {
+            lastMessageID: "zz753fca-e8c3-473b-8e85-b14196e84e16",
+          },
+        })
+      );
+
+      if (!newChatRoomData.data) {
+        console.log(" Failed to create a chat room");
+        return;
+      }
+
+      const newChatRoom = newChatRoomData.data.createChatRoom;
+
+      // 2. Add `user` to the Chat Room
+      await API.graphql(
+        graphqlOperation(createChatRoomUser, {
+          input: {
+            userID: user.id,
+            chatRoomID: newChatRoom.id,
+          },
+        })
+      );
+
+      //  3. Add authenticated user to the Chat Room
+      const userInfo = await Auth.currentAuthenticatedUser();
+      await API.graphql(
+        graphqlOperation(createChatRoomUser, {
+          input: {
+            userID: userInfo.attributes.sub,
+            chatRoomID: newChatRoom.id,
+          },
+        })
+      );
+
+      navigation.navigate("ChatRoom", {
+        id: newChatRoom.id,
+        name: "Hardcoded name",
+      });
+    } catch (e) {
+      console.log(e);
+    }
   };
 
   return (
